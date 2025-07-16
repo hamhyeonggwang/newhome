7746625444da9be2a989a8be32cb5171587a5081
hamhyeonggwang <h2g0614@gmail.com>
2025-07-16 09:26:08 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             R.OTi Lab
           </h1>
         </div>
-        <p className="text-3xl sm:text-4xl font-bold text-[#B0B3B8] mt-2 drop-shadow">빛나는 작업치료사의 <br/> 신나는 지식연구소</p>
+        <p className="text-3xl sm:text-4xl font-bold text-[#B0B3B8] mt-2 drop-shadow"> 빛.나는 작업치료사의 <br/> 신.나는 지식연구소</p>
         <p className="text-xl sm:text-2xl text-[var(--text-sub)] mt-4 font-semibold leading-relaxed text-center max-w-2xl">IT와 작업치료의 만남, 교육과 커뮤니티 <br/>전문성과 따뜻함을 모두 담은 공간</p>
       </section>
 
